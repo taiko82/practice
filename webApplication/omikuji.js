3f66c3d959c61f582fafe105745e3f06c749939b
pycys <51740642+pycys@users.noreply.github.com>
2021-11-19 15:45:34 +0900

    Initial Commit

---
+++ webApplication/omikuji.js
@@ -0,0 +1,39 @@
+let username;
+let userresult;
+
+username = prompt("お名前を教えてください。");
+
+if (username == "") { username = "名無し"; }
+
+document.getElementById("name").innerHTML = username;
+
+let rand = Math.floor(Math.random() * 5);
+
+switch (rand) {
+    
+    case 0:
+        userresult = "大吉";
+        break;
+
+    case 1:
+        userresult = "中吉";
+        break;
+    
+    case 2:
+        userresult = "小吉";
+        break;
+
+    case 3:
+        userresult = "吉";
+        break;
+
+    case 4:
+        userresult = "凶";
+        break;
+
+    default:
+        break;
+
+}
+
+document.getElementById("result").innerHTML = userresult;
